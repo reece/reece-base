7e5747122cc7b0fe5c1e03ce6bf0325afb914da3
Reece Hart <reecehart@gmail.com>
2012-02-12 07:06:07 -0800

    added mendeley-pubmed-filer, image-set-timestamp

--- lib/emacs/init/python-mode.el
+++ lib/emacs/init/python-mode.el
@@ -6,7 +6,7 @@
 ;(autoload 'python-mode "python-mode" "" t)
 
 (defun reece-python-mode-setup ()
-  ; (local-set-key (kbd "C-h f") 'hohe2-lookup-pydoc))
+  (local-set-key (kbd "C-h f") 'hohe2-lookup-pydoc)
   ; (local-set-key (kbd "C-h f") 'nikokrock_pydoc))
   (setq
    indent-tabs-mode		nil)
@@ -25,24 +25,24 @@
 ;===============================================================================
 
 
+;;----pydoc lookup----
+(defun hohe2-lookup-pydoc ()
+  (interactive)
+  (let ((curpoint (point)) (prepoint) (postpoint) (cmd))
+    (save-excursion
+      (beginning-of-line)
+      (setq prepoint (buffer-substring (point) curpoint)))
+    (save-excursion
+      (end-of-line)
+      (setq postpoint (buffer-substring (point) curpoint)))
+    (if (string-match "[_a-z][_\\.0-9a-z]*$" prepoint)
+        (setq cmd (substring prepoint (match-beginning 0) (match-end 0))))
+    (if (string-match "^[_0-9a-z]*" postpoint)
+        (setq cmd (concat cmd (substring postpoint (match-beginning 0) (match-end 0)))))
+    (if (string= cmd "") nil
+      (let ((max-mini-window-height 0))
+        (shell-command (concat "pydoc " cmd))))))
 
-;; ;;----pydoc lookup----
-;; (defun hohe2-lookup-pydoc ()
-;;   (interactive)
-;;   (let ((curpoint (point)) (prepoint) (postpoint) (cmd))
-;;     (save-excursion
-;;       (beginning-of-line)
-;;       (setq prepoint (buffer-substring (point) curpoint)))
-;;     (save-excursion
-;;       (end-of-line)
-;;       (setq postpoint (buffer-substring (point) curpoint)))
-;;     (if (string-match "[_a-z][_\\.0-9a-z]*$" prepoint)
-;;         (setq cmd (substring prepoint (match-beginning 0) (match-end 0))))
-;;     (if (string-match "^[_0-9a-z]*" postpoint)
-;;         (setq cmd (concat cmd (substring postpoint (match-beginning 0) (match-end 0)))))
-;;     (if (string= cmd "") nil
-;;       (let ((max-mini-window-height 0))
-;;         (shell-command (concat "pydoc " cmd))))))
 ;; 
 ;; (defun nikokrock_pydoc (&optional arg)
 ;;   (interactive)
