78f492bc759d88c82392314fbea584874bcda0b0
Reece Hart <reecehart@gmail.com>
2012-10-17 09:31:03 -0700

    periodic sync

--- lib/emacs/init.el
+++ lib/emacs/init.el
@@ -14,6 +14,7 @@
 ;(load-library "init/elisp-mode")
 ;(and window-system (not running-xemacs)
 ;     (load-library "init/font-lock"))
+(load-library "init/ess")
 (load-library "init/modeline")
 (load-library "init/perl-mode")
 (load-library "init/javascript-mode")
@@ -61,7 +62,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
+ '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
  '(cperl-hash-face ((((class color) (background light)) (:foreground "Red" :bold t))))
  '(info-node ((t (:foreground "red" :bold t))))
  '(man-italic ((t (:foreground "red" :bold nil)))))
