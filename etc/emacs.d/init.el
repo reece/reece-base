08f4e9b7772a7f15a010df6b52ff1c4f17f1d958
Reece Hart <reecehart@gmail.com>
2014-06-03 08:36:30 -0700

    more etc/emacs.d/ reorg

--- etc/emacs.d/init.el
+++ etc/emacs.d/init.el
@@ -1,53 +1,30 @@
-; -*-emacs-lisp-*-
-
-(setq user-emacs-dir "~/.emacs.d")
-(add-to-list 'load-path user-emacs-dir)
-
-(load-library "init/package-install")
-
-(require 'tramp)
-
-(require 'auto-complete)
-(require 'auto-complete-config)
-(setq ac-dictionary-files (list (concat user-emacs-directory ".dict")))
-(ac-config-default)
-
-(load-library "pkgs/Rtrinkets")
-(load-library "pkgs/shebang")
-
-(load-library "init/python-mode")
-
-(load-library "init/keys,vars,misc")	; should be decomposed
-
-(yas-global-mode 1)
-;(yas-load-directory)
-
-(server-start)
+(require 'package)
+(add-to-list 'package-archives '("elpy"      . "http://jorgenschaefer.github.io/packages/"))
+(add-to-list 'package-archives '("gnu"       . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa"     . "http://melpa.milkbox.net/packages/"))
+(package-initialize)
 
 
-;; ############################################################################
+(elpy-enable)
+(elpy-use-ipython)
+(pyvenv-workon "default-2.7")
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
+ '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
  '(background-color "#002b36")
  '(background-mode dark)
- '(bookmark-default-file "~/.emacs.d/emacs.bmk")
- '(column-number-mode t)
  '(cursor-color "#839496")
- '(custom-enabled-themes (quote (adwaita)))
- '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
- '(foreground-color "#839496")
- '(inhibit-startup-screen t)
- '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("melpa" . "http://melpa.milkbox.net/packages/") ("marmalade" . "http://marmalade-repo.org/packages/"))))
- '(py-underscore-word-syntax-p nil)
- '(safe-local-variable-values (quote ((encoding . utf-8)))))
+ '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "2b5aa66b7d5be41b18cc67f3286ae664134b95ccc4a86c9339c886dfd736132d" "ed81411169b1b3e3d4cfc39b09d68ea13e0ff7708dc5b9d0bedb319e071968ad" "7a9f392481b6e2fb027ab9d8053ab36c0f23bf5cc1271206982339370d894c74" "c2cfe2f1440d9ef4bfd3ef4cf15bfe35ff40e6d431264b1e24af64f145cffb11" default)))
+ '(foreground-color "#839496"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 78 :width normal)))))
+ )
