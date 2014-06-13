268492483f28edd961d27703a4ad2893fc7fa2c6
Reece Hart <reecehart@gmail.com>
2014-06-13 06:43:07 -0700

    various emacs init updates

--- etc/emacs.d/init.el
+++ etc/emacs.d/init.el
@@ -33,8 +33,7 @@
  '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "2b5aa66b7d5be41b18cc67f3286ae664134b95ccc4a86c9339c886dfd736132d" "ed81411169b1b3e3d4cfc39b09d68ea13e0ff7708dc5b9d0bedb319e071968ad" "7a9f392481b6e2fb027ab9d8053ab36c0f23bf5cc1271206982339370d894c74" "c2cfe2f1440d9ef4bfd3ef4cf15bfe35ff40e6d431264b1e24af64f145cffb11" default)))
  '(foreground-color "#839496")
  '(inhibit-startup-screen t)
-
-)
+ '(safe-local-variable-values (quote ((encoding . utf-8)))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- etc/emacs.d/init/functions.el
+++ etc/emacs.d/init/functions.el
@@ -1,9 +1,21 @@
 ; functions
 ; (see bindings.el for key and mouse bindings, if any)
 
+
 ; adapted from http://www.tardis.ed.ac.uk/~skx/win/.emacs
 (defun my-exit-from-emacs()
   (interactive)
   (if (yes-or-no-p "Do you want to exit ")
       (save-buffers-kill-emacs)))
 
+
+(defun sort-words-in-region ()
+  (interactive)
+  (insert (mapconcat
+		   'identity
+		   (sort (split-string (delete-and-extract-region (point) (mark)))
+				 'string<
+				 )
+		   " ")
+          "\n")
+  ) 
