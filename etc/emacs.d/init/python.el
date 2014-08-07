4e75519d1269da6ce48d370cc9f5da5aaaaeb54a
Reece Hart <reecehart@gmail.com>
2014-08-07 10:13:32 -0500

    periodic commit

--- etc/emacs.d/init/python.el
+++ etc/emacs.d/init/python.el
@@ -1,10 +1,6 @@
 ; downloaded from:
 ; https://launchpad.net/python-mode/trunk/6.1.1/+download/python-mode.el-6.1.1.tar.gz
 
-(elpy-enable)
-(elpy-use-ipython)
-(pyvenv-workon "default-2.7")
-
 (add-to-list 'auto-mode-alist '("\\.wsgi$" . python-mode))
 
 ; shebang lines (when suffixes aren't used)
@@ -16,30 +12,40 @@
 (add-to-list 'interpreter-mode-alist '("python3.4" . python-mode))
 
 (setq-default
- indent-tabs-mode nil
- ;tab-width 4
- )
+;;    elpy-rpc-backend "jedi"
+;;  indent-tabs-mode nil
+;;    jedi:complete-on-dot t
+;;    jedi:server-args      '("--virtual-env" "/home/reece/.virtualenvs/default-2.7")
+;;  tab-width 4
+)
+
+(require 'python-mode)
+
+(pyvenv-workon "default-2.7")
+
+(elpy-enable)
+(elpy-use-ipython)
+
+(add-hook 'elpy-mode-hook 'jedi:setup)
+(add-hook 'python-mode-hook 'pyvenv-mode)
+(add-hook 'jedi-mode-hook 'jedi-direx:setup)
+
+(eval-after-load "python"
+  '(define-key python-mode-map "\C-cx" 'jedi-direx:pop-to-buffer))
 
 
+
+
+
+;; (add-hook 'python-mode-hook 'eldoc-mode)
+
+;; (add-hook 'python-mode-hook 'anaconda-mode)
+;; (add-hook 'python-mode-hook 'auto-complete-mode)
 
 ;; (setq
 ;;  py-docstring-style "DJANGO"
-;;  py-install-directory (concat user-emacs-dir "/pkgs/python-mode.el-6.1.1/")
 ;;  )
-;; (add-to-list 'load-path py-install-directory)
-;; (require 'python-mode)
 ;; 
-;; 
-;; ; jedi
-;; (autoload 'jedi:setup "jedi" nil t)
-;; (setq
-;;  jedi:complete-on-dot t
-;;  jedi:server-args     '("--virtual-env" "/home/reece/.virtualenvs/default-2.7")
-;; )
-;; (add-hook 'python-mode-hook 'jedi:setup)
-;; 
-;; (add-hook 'python-mode-hook 'auto-complete-mode)
-
 ;; ;; pyflakes flymake integration
 ;; ;; http://stackoverflow.com/a/1257306/347942
 ;; (when (load "flymake" t)
